883e69257c06adbb67a516d5ee3448ee74585e24
Dylan Moo Hernandez <dylangmooh@gmail.com>
2025-03-10 16:30:38 -0400

    Initial commit: Add LunarRover, SpaceMaster, SRFP

---
+++ LunarRover/src/model/HLAPositonCoder.java
@@ -0,0 +1,53 @@
+package model;
+
+import hla.rti1516e.RtiFactoryFactory;
+import hla.rti1516e.encoding.DecoderException;
+import hla.rti1516e.encoding.EncoderFactory;
+import hla.rti1516e.encoding.HLAfixedRecord;
+import hla.rti1516e.encoding.HLAfloat64LE;
+import hla.rti1516e.exceptions.RTIinternalError;
+import skf.coder.Coder;
+
+public class HLAPositonCoder implements Coder<Position> {
+	
+	private EncoderFactory factory = null;
+	private HLAfixedRecord coder = null;
+	
+	private HLAfloat64LE x;
+	private HLAfloat64LE y;
+	private HLAfloat64LE z;
+	
+	public HLAPositonCoder() throws RTIinternalError {
+		this.factory = RtiFactoryFactory.getRtiFactory().getEncoderFactory();
+		this.coder = factory.createHLAfixedRecord();
+		
+		this.x = factory.createHLAfloat64LE();
+		this.y = factory.createHLAfloat64LE();
+		this.z = factory.createHLAfloat64LE();
+		
+		coder.add(x);
+		coder.add(y);
+		coder.add(z);
+		
+	}
+
+	@Override
+	public Position decode(byte[] arg0) throws DecoderException {
+		coder.decode(arg0);
+		return new Position(x.getValue(), y.getValue(), z.getValue());
+	}
+
+	@Override
+	public byte[] encode(Position arg0) {
+		this.x.setValue(arg0.getX());
+		this.y.setValue(arg0.getY());
+		this.z.setValue(arg0.getZ());
+		return coder.toByteArray();
+	}
+
+	@Override
+	public Class<Position> getAllowedType() {
+		return Position.class;
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# SEE2025 Federates
+
+## Installation
+
+1. Clone this repository: https://github.com/moodylan/SEE2025-UCF-Federates.git
+2. Install the required dependencies.
+3. Place the missing Pitch license JAR files inside `lib/`.
+4. Run the federates in this order:
+- SpaceMaster
+- SpaceReferenceFramePublisher
+- LunarRover
+
+## Missing Files
+- Your Pitch license JAR files are not included. Please place them manually in the `lib/` folder.
